6fb7740efd388c0ce17fba8b648394ddbc6e22e7
jtiagotoledo <jtiagotoledo@gmail.com>
2026-08-30 09:03:34 -0300

    aplicado o jwt nas rotas admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 const adminController = require('../controllers/adminController');
 
+const authMiddleware = require('../middlewares/authMiddleware');
+
 router.post('/', adminController.criarAdmin);
-router.get('/', adminController.listarAdmins);
-router.get('/:id', adminController.buscarAdminPorId);
-router.put('/:id', adminController.atualizarAdmin);
-router.delete('/:id', adminController.deletarAdmin);
+
+router.get('/', authMiddleware, adminController.listarAdmins);
+router.get('/:id', authMiddleware, adminController.buscarAdminPorId);
+router.put('/:id', authMiddleware, adminController.atualizarAdmin);
+router.delete('/:id', authMiddleware, adminController.deletarAdmin);
 
 module.exports = router;
