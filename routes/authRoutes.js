bb53de7a077bdf3ca7f95876ffe2fbe07dc9f420
Tiago Toledo <jtiagotoledo@gmail.com>
2026-09-04 09:27:53 -0300

    sitema de troca de senha temp v2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const autenticarToken = require('../middlewares/authMiddleware');
 
 router.post('/login', authController.login);
 router.post('/trocar-senha-primeiro-acesso', autenticarToken, authController.trocarSenhaPrimeiroAcesso);
