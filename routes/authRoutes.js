61da911e062597ac7e76c4e6893dbb1d5e9bd03d
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2026-09-04 14:13:08 -0300

    sistema para esqueci senha v2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,8 @@ const authController = require('../controllers/authController');
 const autenticarToken = require('../middlewares/authMiddleware');
 
 router.post('/login', authController.login);
+router.post('/solicitar-recuperacao', authController.solicitarRecuperacaoSenha);
+router.post('/redefinir-senha', authController.redefinirSenha);
 router.post('/trocar-senha-primeiro-acesso', autenticarToken, authController.trocarSenhaPrimeiroAcesso);
 
 module.exports = router;
