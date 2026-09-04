c8e187c0de97a77fea32ae9ce8b059bea9e0e1be
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2026-09-04 14:28:32 -0300

    sistema para esqueci senha v3

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admins', adminRoutes);
 app.use('/api/professores', professorRoutes);
 app.use('/api/alunos', alunoRoutes);
-app.use(express.static('public'));
 
 mongoose.connect(MONGO_URI)
   .then(() => {
